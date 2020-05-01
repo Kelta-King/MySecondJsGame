305f432c54f586c36293b2c0d1005eb39e5e619e
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-01 14:19:40 +0530

    All the classes related to games

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var canvas=document.getElementById("myCanvas");
 var ctx= canvas.getContext("2d");
-		    
+
+//variables
 var bird_1='https://i.ibb.co/DkW7qrs/Bird-1.png';
 var bird_2='https://i.ibb.co/LRfyQ18/Bird-2.png';
 var bird_dead='https://imgur.com/download/beMAg0Q';
@@ -25,5 +26,110 @@ var Scr=document.getElementById("score");
 Scr.innerHTML="Score: "+score;
 var Arrs=document.getElementById("arrows");
 Scr.innerHTML="Remaining Arrows: "+arrs;
-var m=1;//m=1 wallpapaer m=0 for game
+var m=1;
+//m=1 wallpapaer m=0 for game
+
+
+//classes
+		    
+class Thing
+{
+    img;
+    x;
+    y;
+    constructor(src,x,y)
+     {
+        this.img=new Image();
+        this.img.src=src;
+        this.x=x;
+        this.y=y;
+     }
+                setWidth(width)
+                {
+                    this.img.width=100+"px";
+                }
+            }
+            
+            class sound
+            {
+                sound;
+                constructor(src)
+                {
+                    this.sound=document.createElement('audio');
+                    this.sound.src=src;
+                    this.sound.setAttribute("preload","auto");
+                    this.sound.setAttribute("controls","none");
+                    this.sound.style.display="none";
+                }
+                play()
+                {
+                    this.sound.play();
+                }
+                stop()
+                {
+                    this.sound.pause();
+                }
+                decVol()
+                {
+                	if(this.volume>=0)
+                    {
+                		this.volume-=20;
+                    }
+                }
+                incVol()
+                {
+                	if(this.volume<=100)
+                    {
+                		this.volume+=20;
+                    }
+                }
+            }
+            
+            
+            class Draw
+		    {
+		        static floor(x,y,width,height,color)
+		        {
+		            ctx.beginPath();
+		            ctx.rect(x,y,width,height);
+		            ctx.fillStyle=color;
+		            ctx.fill();
+		            ctx.closePath();
+		        }
+		        
+		        static drawRotatedB(degrees,image,canvas)
+		        {
+                    ctx.save();
+                    ctx.translate(100,canvas.height-200);
+                    ctx.rotate(degrees*Math.PI/180);
+                    ctx.drawImage(image,-image.width/2,-image.width/2);
+                    ctx.restore();
+                }
+                
+                static drawRotatedA(degrees,image,canvas)
+		        {
+                    ctx.save();
+                    ctx.translate(-455,65);
+                    ctx.rotate(degrees*Math.PI/180);
+                    ctx.drawImage(image,0,canvas.height,400,15);
+                    
+                    ctx.restore();
+                }
+		        
+		    }
+            
+            
+            class forRandom
+		    {
+		        n;
+		        static rand()
+		        {
+		            this.n=Math.ceil(Math.random()*500);
+                    if(this.n>=10)
+                    {
+                        return this.n;
+                    }
+                    return null;
+		        }
+		    }
 		    
